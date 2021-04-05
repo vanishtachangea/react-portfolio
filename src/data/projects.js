1b100c51ac1304900515f16076bbcd876cc32301
vanishta <vanishta.changea@gmail.com>
2021-04-05 16:51:17 +0200

    Add musicBar

--- src/data/projects.js
+++ src/data/projects.js
@@ -1,35 +1,42 @@
-import project1 from '../assets/project1.png';
-import project2 from '../assets/project2.png';
-import project3 from '../assets/project3.png';
-import project4 from '../assets/project4.png';
-import tetris from '../assets/tetris.png';
-const PROJECTS = [
-    {
-        id: 1,
-        title: "Travel Blog",
-        description: 'My First React Application developed in 2018',
-        link: "http://enrichingtravels.com/",
-        GitHub: "Private Repo",
-        image: project1
-    },
+import project1 from "../assets/project1.png";
+import project2 from "../assets/project2.png";
+// import project3 from "../assets/project3.png";
+// import project4 from "../assets/project4.png";
+import musicBar from "../assets/musicBar.png";
+import tetris from "../assets/tetris.png";
 
-    {
-        id: 2,
-        title: "Guess Odd or Even Game",
-        description: 'Application built in Redux in 2020',
-        link: "/evenoddgame",
-        image: project2
-    },
-	{
-        id: 3,
-        title: "Tetris Game",
-        description: 'Tetris Game -  React and Custom Hooks in 2020',
-        link: "http://react-tetrisgame.s3-website-us-east-1.amazonaws.com/",
-        image: tetris		
-		
-		
-	}
-/*     ,
+const PROJECTS = [
+  {
+    id: 1,
+    title: "Travel Blog",
+    description: "My First React Application developed in 2018",
+    link: "http://enrichingtravels.com/",
+    GitHub: "Private Repo",
+    image: project1,
+  },
+  {
+    id: 32,
+    title: "Music Bar",
+    description: "Application built in React only in 2021",
+    link: "http://musicbar.s3-website-us-east-1.amazonaws.com/",
+    image: musicBar,
+  },
+
+  {
+    id: 3,
+    title: "Guess Odd or Even Game",
+    description: "Application built in Redux in 2020",
+    link: "/evenoddgame",
+    image: project2,
+  },
+  {
+    id: 4,
+    title: "Tetris Game",
+    description: "Tetris Game -  React and Custom Hooks in 2020",
+    link: "http://react-tetrisgame.s3-website-us-east-1.amazonaws.com/",
+    image: tetris,
+  },
+  /*     ,
     {
         id: 3,
         title: "Interactive tic-tac-toe game",
